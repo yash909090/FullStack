54fa6c8746ff4cd0a380fb9d6092948b4216ccb0
yash909090 <yashkakadiya2000@gmail.com>
2022-05-10 11:52:34 +0530

    Update regEx.js

--- ticketTask_MERN/client/src/js/regEx.js
+++ ticketTask_MERN/client/src/js/regEx.js
@@ -1,5 +1,5 @@
 export const validName = new RegExp('^[a-zA-Z ]+$');
-export const validUserName = new RegExp("^([A-Za-z_][A-Za-z0-9_]*).{5,30}$");
+export const validUserName = new RegExp("^([A-Za-z_][A-Za-z0-9_]*){8,30}$");
 export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,12}$');
 
 
